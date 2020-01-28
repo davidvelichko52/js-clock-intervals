6ea0d28de82eac2ea10107cc43a4d18162a49bf5
davidvelichko52 <davidvelichko52@gmail.com>
2020-01-27 18:21:47 -0800

    clock homework

---
+++ clock.js
@@ -0,0 +1,21 @@
+let secondDegrees = 360/60;
+let minuteDegrees = 360/60/60/60;
+let hourDegrees = 360/60/60;
+
+var second = document.getElementById("second");
+var minute = document.getElementById("minute");
+var hour = document.getElementById("hour");
+
+
+
+
+let clock = setInterval(function() {
+    console.log("loopie bois");  
+    second.style.transform = "rotate(" + secondDegrees + "deg)";
+    secondDegrees = secondDegrees + 360/60;
+    minute.style.transform = "rotate(" + minuteDegrees + "deg)";
+    minuteDegrees = minuteDegrees + 360/60/60/60;
+    hour.style.transform = "rotate(" + hourDegrees + "deg)";
+    hourDegrees = hourDegrees + 360/60/60;
+}, 1000);
+
